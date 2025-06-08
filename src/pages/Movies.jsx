d935dd29170d9ff2a76ffbbd2f325d1d0142267d
gilmerperez <gilmer2002@outlook.com>
2025-06-08 17:20:29 -0400

    Added search bar

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -11,6 +11,120 @@ function Movies() {
         <div className={`container ${styles.container}`}>
           {/* Heading */}
           <h1 className={styles.heading}>Movies</h1>
+          {/* Search Bar */}
+          <section className={styles.searchBar}>
+            <input type="text" placeholder="Enter Keywords..." />
+            <button>
+              <i class="fa-solid fa-film"></i>
+            </button>
+          </section>
+          {/* Movie Cards */}
+          <section className={styles.movieCards}>
+            <div className={styles.movieCard}>
+              <img src="src/assets/images/alien-romulus-movie-poster.jpg" alt="Movie Name Poster" />
+              <div className={styles.movieCardContent}>
+                <h2 className={styles.movieTitle}>Alien: Romulus</h2>
+                <h3 className={styles.movieYear}>2024</h3>
+                <h3 className={styles.movieLength}>1h 59m</h3>
+                <h3 className={styles.movieRating}>R</h3>
+                <p className={styles.movieDescription}>
+                  Space colonizers come face to face with the most terrifying life-form in the universe while scavenging
+                  the deep ends of a derelict space station.
+                </p>
+              </div>
+            </div>
+            <div className={styles.movieCard}>
+              <img src="src/assets/images/alien-romulus-movie-poster.jpg" alt="Movie Name Poster" />
+              <div className={styles.movieCardContent}>
+                <h2 className={styles.movieTitle}>Alien: Romulus</h2>
+                <h3 className={styles.movieYear}>2024</h3>
+                <h3 className={styles.movieLength}>1h 59m</h3>
+                <h3 className={styles.movieRating}>R</h3>
+                <p className={styles.movieDescription}>
+                  Space colonizers come face to face with the most terrifying life-form in the universe while scavenging
+                  the deep ends of a derelict space station.
+                </p>
+              </div>
+            </div>
+            <div className={styles.movieCard}>
+              <img src="src/assets/images/alien-romulus-movie-poster.jpg" alt="Movie Name Poster" />
+              <div className={styles.movieCardContent}>
+                <h2 className={styles.movieTitle}>Alien: Romulus</h2>
+                <h3 className={styles.movieYear}>2024</h3>
+                <h3 className={styles.movieLength}>1h 59m</h3>
+                <h3 className={styles.movieRating}>R</h3>
+                <p className={styles.movieDescription}>
+                  Space colonizers come face to face with the most terrifying life-form in the universe while scavenging
+                  the deep ends of a derelict space station.
+                </p>
+              </div>
+            </div>
+            <div className={styles.movieCard}>
+              <img src="src/assets/images/alien-romulus-movie-poster.jpg" alt="Movie Name Poster" />
+              <div className={styles.movieCardContent}>
+                <h2 className={styles.movieTitle}>Alien: Romulus</h2>
+                <h3 className={styles.movieYear}>2024</h3>
+                <h3 className={styles.movieLength}>1h 59m</h3>
+                <h3 className={styles.movieRating}>R</h3>
+                <p className={styles.movieDescription}>
+                  Space colonizers come face to face with the most terrifying life-form in the universe while scavenging
+                  the deep ends of a derelict space station.
+                </p>
+              </div>
+            </div>
+            <div className={styles.movieCard}>
+              <img src="src/assets/images/alien-romulus-movie-poster.jpg" alt="Movie Name Poster" />
+              <div className={styles.movieCardContent}>
+                <h2 className={styles.movieTitle}>Alien: Romulus</h2>
+                <h3 className={styles.movieYear}>2024</h3>
+                <h3 className={styles.movieLength}>1h 59m</h3>
+                <h3 className={styles.movieRating}>R</h3>
+                <p className={styles.movieDescription}>
+                  Space colonizers come face to face with the most terrifying life-form in the universe while scavenging
+                  the deep ends of a derelict space station.
+                </p>
+              </div>
+            </div>
+            <div className={styles.movieCard}>
+              <img src="src/assets/images/alien-romulus-movie-poster.jpg" alt="Movie Name Poster" />
+              <div className={styles.movieCardContent}>
+                <h2 className={styles.movieTitle}>Alien: Romulus</h2>
+                <h3 className={styles.movieYear}>2024</h3>
+                <h3 className={styles.movieLength}>1h 59m</h3>
+                <h3 className={styles.movieRating}>R</h3>
+                <p className={styles.movieDescription}>
+                  Space colonizers come face to face with the most terrifying life-form in the universe while scavenging
+                  the deep ends of a derelict space station.
+                </p>
+              </div>
+            </div>
+            <div className={styles.movieCard}>
+              <img src="src/assets/images/alien-romulus-movie-poster.jpg" alt="Movie Name Poster" />
+              <div className={styles.movieCardContent}>
+                <h2 className={styles.movieTitle}>Alien: Romulus</h2>
+                <h3 className={styles.movieYear}>2024</h3>
+                <h3 className={styles.movieLength}>1h 59m</h3>
+                <h3 className={styles.movieRating}>R</h3>
+                <p className={styles.movieDescription}>
+                  Space colonizers come face to face with the most terrifying life-form in the universe while scavenging
+                  the deep ends of a derelict space station.
+                </p>
+              </div>
+            </div>
+            <div className={styles.movieCard}>
+              <img src="src/assets/images/alien-romulus-movie-poster.jpg" alt="Movie Name Poster" />
+              <div className={styles.movieCardContent}>
+                <h2 className={styles.movieTitle}>Alien: Romulus</h2>
+                <h3 className={styles.movieYear}>2024</h3>
+                <h3 className={styles.movieLength}>1h 59m</h3>
+                <h3 className={styles.movieRating}>R</h3>
+                <p className={styles.movieDescription}>
+                  Space colonizers come face to face with the most terrifying life-form in the universe while scavenging
+                  the deep ends of a derelict space station.
+                </p>
+              </div>
+            </div>
+          </section>
         </div>
       </main>
     </>
