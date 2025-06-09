63342b4b5f40419aef8aba9d81baeae231398981
gilmerperez <gilmer2002@outlook.com>
2025-06-09 16:43:51 -0400

    Added better styles to fit individual movie content

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,12 +10,14 @@ function MovieCard({ movie }) {
         <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} Poster`} />
         <div className={styles.movieCardContent}>
           <h2 className={styles.movieTitle}>{movie.title}</h2>
-          <h3 className={styles.movieYear}>{movie.release_date?.split("-")[0]}</h3>
-          <h3 className={styles.movieRating}>{movie.certification}</h3>
-          <h3 className={styles.movieLength}>
-            {hours > 0 ? `${hours}h ` : ""}
-            {minutes}m
-          </h3>
+          <div className={styles.movieMeta}>
+            <h3 className={styles.movieYear}>{movie.release_date?.split("-")[0]}</h3>
+            <h3 className={styles.movieRating}>{movie.certification}</h3>
+            <h3 className={styles.movieLength}>
+              {hours > 0 ? `${hours}h ` : ""}
+              {minutes}m
+            </h3>
+          </div>
           <h3 className={styles.movieGenre}>{movie.genre_names.join(" / ")}</h3>
           <p className={styles.movieDescription}>{movie.overview}</p>
         </div>
