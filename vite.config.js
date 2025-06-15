3effcb6f092533e3d29c886e5dccdfd477943a77
gilmerperez <gilmer2002@outlook.com>
2025-06-15 15:52:37 -0400

    Cahnged vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, process } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    preview: {
+      port: parseInt(env.PORT) || 4173,
+      host: true,
+      allowedHosts: ["reelix.onrender.com"],
+    },
+  };
 });
